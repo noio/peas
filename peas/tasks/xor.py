d48b4d84286bfc486dbf4adb2858a14ecd41d789
Thomas van den Berg <paranoid.ndroid@gmail.com>
2012-07-06 17:41:00 +0200

    add xor task

---
+++ peas/tasks/xor.py
@@ -0,0 +1,56 @@
+""" Input/output relation task. Every input and output
+    is explicitly defined. XOR is an example of this task.
+"""
+
+### IMPORTS ###
+import random
+
+# Libraries
+import numpy as np
+
+# Local
+from ..networks.rnn import NeuralNetwork
+
+
+class XORTask(object):
+    
+    # Default XOR input/output pairs
+    INPUTS  = [(0,0), (0,1), (1,0), (1,1)]
+    OUTPUTS = [(0,), (1,), (1,), (0,)]
+    EPSILON = 1e-100
+    
+    def __init__(self, do_all=True):
+        self.do_all = do_all
+        self.INPUTS = np.array(self.INPUTS, dtype=float)
+        self.OUTPUTS = np.array(self.OUTPUTS, dtype=float)
+    
+    def evaluate(self, network, verbose=False):
+        if not isinstance(network, NeuralNetwork):
+            network = NeuralNetwork(network)
+        
+        network.make_feedforward()
+        
+        pairs = zip(self.INPUTS, self.OUTPUTS)
+        random.shuffle(pairs)
+        if not self.do_all:
+            pairs = [random.choice(pairs)]
+        rmse = 0.0
+        for (i, target) in pairs:
+            # Feed with bias
+            output = network.feed(i)
+            # Grab the output
+            output = output[-len(target):]
+            err = (target - output)
+            err[abs(err) < self.EPSILON] = 0;
+            err = (err ** 2).mean()
+            # Add error
+            if verbose:
+                print "%r -> %r (%.2f)" % (i, output, err)
+            rmse += err 
+
+        return 1/(1+np.sqrt(rmse / len(pairs)))
+        
+    def solve(self, network):
+        return int(self.evaluate(network) > 0.9)
+                
+        
